34608a9fdd1a9b0380e7678355a1b6552b5247b6
Samuel <samueld79@outlook.com>
2026-03-17 15:15:33 -0500

    docs: alinear repo con Agency OS actual

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Calendar, RefreshCw, Plus } from 'lucide-react';
-import { CalendarWidget } from '../components/CalendarWidget';
 
 type CalEvent = {
   id: string;
